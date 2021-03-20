1e22536cc5a67f89aa94451ddaa7b9812b108716
Cristian Vargas <cvg97@misena.edu.co>
2021-03-20 18:16:14 -0500

    remove code and unecessary files

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductController } from './product/product.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
